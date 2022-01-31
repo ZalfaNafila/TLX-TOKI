488892e41d1cc06b48e5460f6c0b88b4875c11f9
zalfa <92864261+ZalfaNafila@users.noreply.github.com>
2022-01-31 11:12:40 +0700

    don't overthink about this problem..
    it's pretty simple to solve if you understand about this problem..

---
+++ TROC#21A - Lit Hairstyle.cpp
@@ -0,0 +1,15 @@
+//link : https://tlx.toki.id/problems/troc-21/A
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n,d;
+	cin >> n >> d;
+	if(d == 1){
+		cout << "NO";
+	}
+	else{
+		cout << "YES";
+	}
+}
