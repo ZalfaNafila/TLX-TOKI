ee2734de5712445cba50050a059580c4d691c2af
zalfa <92864261+ZalfaNafila@users.noreply.github.com>
2022-01-31 12:50:11 +0700

    Firework Festival

---
+++ TROC#17.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int n,a[101],b[101];
+	int sum = 0;
+	cin >> n;
+	for(int i=1; i<=n; i++){
+		cin >> a[i];
+		sum+=a[i];
+		}
+	for(int j=1; j<=n; j++){
+		cin >> b[j];
+	}
+	if(sum % 2 == 0){
+		cout << "0";
+	}
+	else
+	cout << "1";
+}
